b4294c19e313e0013c4dfcfd2e2b100df0b5d4d8
DaNaCampean <campean.dana@gmail.com>
2025-04-30 17:34:30 +0300

    wind apra

---
+++ src/HW_01_emag/testCases/feed.java
@@ -0,0 +1,27 @@
+package HW_01_emag.testCases;
+
+public class feed {
+
+/*
+.idea, .iml nu se pun la commit
+
+scriu aici ca sa ramana si pt ceilalti
+
+locatorii se pun sus, nu inainte de fiecare clasa unde e folosit
+
+(sus, adica deasupra primei metode)
+
+// methode for assert
+
+    public static void verificationAssert(String actualResult, String expectedResult, String verifText){
+
+        assertEquals(actualResult, expectedResult, verifText);
+
+    } asta e redundanta, ca nu faci nimic in plus peste acel assertequals sa ti se merite
+
+clasele de Page, eg. HomePage, trebe puse intr=un BaseTest si fiecare test sa extinda basetest
+
+ */
+
+
+}
